d2f8c396bd747d8a94cba54e35bd2b3beb0b2c64
pranksterinnsut <135349194+pranksterinnsut@users.noreply.github.com>
2023-06-03 12:50:24 +0530

    Update faulty_pointer.py

--- files/faulty_pointer.py
+++ files/faulty_pointer.py
@@ -3,7 +3,7 @@ from time import *
 from pynput import keyboard
 from random import *
 
-l = ["up","down","right","left","up","down","up","down","up","down","up","down","up","down"]
+l = ["up","down","right","left","enter","up","enter","down","enter","up","down","up","down","up","down","up","down","enter"]
 
 def check():
     sleep(15)
