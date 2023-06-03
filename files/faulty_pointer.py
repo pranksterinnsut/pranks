0b2f04d21c1444b4373bdd28821922fe35b2f8c4
pranksterinnsut <135349194+pranksterinnsut@users.noreply.github.com>
2023-06-03 10:15:11 +0530

    Add files via upload

---
+++ files/faulty_pointer.py
@@ -0,0 +1,38 @@
+from pyautogui import *
+from time import *
+from pynput import keyboard
+from random import *
+
+l = ["up","down","right","left","up","down","up","down","up","down","up","down","up","down","up","down","up","down","up","down"]
+
+def check():
+    sleep(15)
+    while(True):
+        sleep(0.3)
+        s = l[randint(0,len(l)-1)]
+        press(s)
+    
+    
+n = 0
+def on_press(key):
+    if key == keyboard.Key.ctrl_l:
+        check()
+    if key == keyboard.Key.ctrl_r:
+        check()
+    if key == keyboard.Key.enter:
+        check()
+    if key == keyboard.Key.shift_l:
+        check()
+    if key == keyboard.Key.shift_r:
+        check()
+    if key == keyboard.Key.tab:
+        check()
+
+# Create a listener
+listener = keyboard.Listener(on_press=on_press)
+
+# Start the listener in a separate thread
+listener.start()
+
+# Keep the main thread running to prevent the script from exiting
+listener.join()
